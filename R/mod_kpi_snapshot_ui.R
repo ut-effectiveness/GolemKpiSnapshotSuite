8ce3bd2289e155f5344ef8604811c47d8bbb8eab
CraigDemke <craig.demke@utahtech.edu>
2025-09-15 16:49:55 -0600

    trying to fix issue

--- R/mod_kpi_snapshot_ui.R
+++ R/mod_kpi_snapshot_ui.R
@@ -1,6 +1,14 @@
 #' KPI Snapshot UI Module
-#' @import shiny
 #' @export
+#' @param id module id
+#' @importFrom shiny NS tagList uiOutput
+kpi_snapshot_inner_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    uiOutput(ns("value_boxes_ui")),
+    uiOutput(ns("plots_ui"))
+  )
+}
 mod_kpi_snapshot_ui <- function(id, custom_ui = NULL) {
   ns <- shiny::NS(id)
   shiny::tagList(
