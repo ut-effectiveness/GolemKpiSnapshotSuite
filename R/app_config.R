bf19ef12461441849cd843bab4a2cbe3401aa513
CraigDemke <craig.demke@utahtech.edu>
2025-09-18 10:11:05 -0600

    fixed the roxygen

--- R/app_config.R
+++ R/app_config.R
@@ -6,14 +6,7 @@
 #' @param use_parent Whether to use parent config logic
 #' @return The config value
 #' @export
-#' Universal golem config getter for parent and child apps
 #'
-#' @param value Config value to retrieve
-#' @param package Name of the package to read config from. Defaults to parent package ("GolemKpiSnapshotSuite").
-#' @param config Active config to use
-#' @param use_parent Whether to use parent config logic
-#' @return The config value
-#' @export
 get_golem_config <- function(
     value,
     package = "GolemKpiSnapshotSuite",
