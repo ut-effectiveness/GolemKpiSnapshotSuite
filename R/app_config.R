1604e898a308c5aeca09ec9ebe84b3788281fe26
CraigDemke <craig.demke@utahtech.edu>
2025-09-18 09:53:26 -0600

    had to rename the package

--- R/app_config.R
+++ R/app_config.R
@@ -1,19 +1,19 @@
-#' Access files in the current app
+#' Universal golem config getter for parent and child apps
+#'
+#' @param value Config value to retrieve
+#' @param package Name of the package to read config from. Defaults to parent package ("GolemKpiSnapshotSuite").
+#' @param config Active config to use
+#' @param use_parent Whether to use parent config logic
+#' @return The config value
 #' @export
-#' @noRd
-app_sys <- function(...) {
-  file.exists(system.file("golem-config.yml", package = "GolemKpiSnapshotSuite"))
-}
-
-#' Read App Config
-#' @export
-#' @noRd
 get_golem_config <- function(
     value,
+    package = "GolemKpiSnapshotSuite", # <-- default to parent package
     config = Sys.getenv("GOLEM_CONFIG_ACTIVE", Sys.getenv("R_CONFIG_ACTIVE", "default")),
-    use_parent = TRUE,
-    file = app_sys("golem-config.yml")
+    use_parent = TRUE
 ) {
+  system.file(..., package = "GolemKpiSnapshotSuite")
+  if (file == "") stop("Config file not found in package: ", package)
   config::get(
     value = value,
     config = config,
