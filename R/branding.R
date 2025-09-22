05236fb526d49cf186e1d20884dbf9370d8ab76a
CraigDemke <craig.demke@utahtech.edu>
2025-09-22 13:42:53 -0600

    troubleshooting custom sytyling

--- R/branding.R
+++ R/branding.R
@@ -1,37 +1,41 @@
-#' Shared KPI branding assets
+#' Shared branding assets
 #' @param prefix resource path prefix
-#' @param include_css include custom.css & litera_style.css
 #' @export
-kpi_branding <- function(prefix = "gkss", include_css = TRUE){
+kpi_branding <- function(prefix = "gkss"){
   root <- system.file("app","www", package = "GolemKpiSnapshotSuite")
   if (root == "" || !dir.exists(root)) return(htmltools::tagList())
   if (is.null(shiny::getResourcePaths()[[prefix]])){
     shiny::addResourcePath(prefix, root)
   }
   deps <- list()
-  if (include_css){
-    for (css in c("custom.css","litera_style.css")){
-      if (file.exists(file.path(root, css))){
-        deps[[css]] <- htmltools::htmlDependency(
-          name = sub("\\.css$","", css),
-          version = "1.0",
-            src = c(href = prefix),
-          stylesheet = css
-        )
-      }
+  for (css in c("litera_style.css","custom.css")){
+    f <- file.path(root, css)
+    if (file.exists(f)){
+      deps[[css]] <- htmltools::htmlDependency(
+        name = sub("\\.css$","", css),
+        version = "1.0",
+        src = c(href = prefix),
+        stylesheet = css
+      )
     }
   }
   fav <- NULL
-  if (file.exists(file.path(root,"favicon.png"))){
-    fav <- htmltools::tags$link(rel="icon", href = sprintf("%s/favicon.png", prefix))
+  for (icon in c("favicon.png","favicon.ico")){
+    if (file.exists(file.path(root, icon))){
+      fav <- htmltools::tags$link(rel="icon", href = sprintf("/%s/%s", prefix, icon))
+      break
+    }
   }
   htmltools::tagList(deps, fav)
 }
 
 #' Logo tag
 #' @param file image filename in inst/app/www
-#' @param prefix resource prefix
 #' @export
 kpi_logo <- function(file = "ie_logo.png", prefix = "gkss", ...){
-  htmltools::tags$img(src = sprintf("%s/%s", prefix, file), alt = "Logo", ...)
+  htmltools::tags$img(
+    src = sprintf("/%s/%s", prefix, file),
+    alt = "Logo",
+    ...
+  )
 }
