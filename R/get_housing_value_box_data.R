7a76d5f5a0a5fb7bee11f580eb10d4c1adbe97c7
CraigDemke <craig.demke@utahtech.edu>
2025-09-24 15:30:44 -0600

    cleaning up parms

--- R/get_housing_value_box_data.R
+++ R/get_housing_value_box_data.R
@@ -1,6 +1,5 @@
 #' Convenience wrapper to produce housing value box data
 #'
-#' @inheritParams housing_value_box_data
 #' @return Tibble of value box metrics.
 #' @export
 get_housing_value_box_data <- function(){
--- R/housing_detailed_table.R
+++ R/housing_detailed_table.R
@@ -1,7 +1,5 @@
 #' Housing detailed table
 #'
-#' @param dsn Optional data source name.
-#' @param context Optional context object.
 #' @return A tibble / data.frame for display.
 #' @export
 housing_detailed_table <- function() {
