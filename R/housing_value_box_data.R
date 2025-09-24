503d88d8c5b9ae10f6cfb4ccb8612a17d958847f
CraigDemke <craig.demke@utahtech.edu>
2025-09-24 15:36:55 -0600

    lets try this now

--- R/housing_value_box_data.R
+++ R/housing_value_box_data.R
@@ -1,10 +1,8 @@
 #' Build housing value box data
 #'
-#' @param dsn Optional data source name / connection (if used).
-#' @param context Optional context/environment object.
 #' @return Tibble ready for value box creation.
 #' @export
 housing_value_box_data <- function() {
-  raw <- housing_pull_raw(dsn = dsn, context = context)
+  raw <- housing_pull_raw()
   housing_build(raw)$final_housing_for_valueboxs
 }
