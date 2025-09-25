ca3153b9ba09ce4186aa01630bb690766a69710c
CraigDemke <craig.demke@utahtech.edu>
2025-09-25 09:09:08 -0600

    trying to clean up the look of the app

--- R/litera_theme.R
+++ R/litera_theme.R
@@ -2,9 +2,10 @@
 #' @export
 litera_theme <- function() {
   bslib::bs_theme(
-    bootswatch = "litera",
-    primary    = "#003058",
-    secondary  = "#a50000",
-    base_font  = bslib::font_google("Helvetica Neue", fallback = "Arial, sans-serif")
+    bootswatch   = "litera",
+    primary      = "#003058",
+    secondary    = "#a50000",
+    base_font    = bslib::font_google("Source Serif Pro"),
+    heading_font = bslib::font_google("Josefin Sans", wght = 300)
   )
 }
